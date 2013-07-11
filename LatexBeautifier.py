d2d7ec6d3b8562e348f939cc1598d3c1a2ad4e4f
Daniel Bugl <daniel.bugl@touchlay.com>
2013-07-11 12:55:55 +0200

    better char limit management

--- LatexBeautifier.py
+++ LatexBeautifier.py
@@ -35,12 +35,13 @@ class LatexBeautifier(LatexDocument):
                 document_buffer += self.__config["LatexText"]["indentation"]
                 i = 0
                 for c in l.getString():
-                    if i >= self.__config["LatexText"]["charlimit"]:
+                    document_buffer += c
+                    i += 1
+                    # TODO: code a better in-word algorithm than 'if c == " "'
+                    if i >= self.__config["LatexText"]["charlimit"] and c == " ":
                         document_buffer += "\n"
                         document_buffer += self.__config["LatexText"]["indentation"]
                         i = 0
-                    document_buffer += c
-                    i += 1
             elif isinstance(l, LatexComment):
                 document_buffer += self.__config["LatexComment"]["indentation"]
                 document_buffer += l.getString()
