04da34311787fd6bbb2cd52e7e83e569e06f3758
Daniel Bugl <daniel.bugl@touchlay.com>
2013-07-22 12:07:18 +0200

    strings in test_parser are raw strings now, should fix the build problems on python3

--- test/test_parser.py
+++ test/test_parser.py
@@ -4,13 +4,13 @@ from latex import LatexParser
 class TestLatexParser:
     def test_parser(self):
         self.parser = LatexParser(self.document)
-        assert self.parser.getResult().getDocument() == """\documentclass[11pt,a4paper,oneside]{report}
+        assert self.parser.getResult().getDocument() == r"""\documentclass[11pt,a4paper,oneside]{report}
 \usepackage{pslatex,palatino,avant,graphicx,color}
 \usepackage[margin=2cm]{geometry}
 % testtesttesttest
-\\begin{document}
-\\title{\color{red}Practical Typesetting}
-\\author{\color{blue}Name}
+\begin{document}
+\title{\color{red}Practical Typesetting}
+\author{\color{blue}Name}
 \date{\color{green}December 2005}
 \maketitle
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus consectetur tellus. Duis vehicula, tortorgravida sollicitudin eleifend, erat eros feugiat nisl, eget ultricies risus magna ac leo. Ut est diam, faucibustincidunt ultrices sit amet, congue sed tellus. Donec vel tellus vitae sem mattis congue. Suspendisse faucibussemper faucibus. Curabitur congue est arcu, nec sollicitudin odio blandit at. Nullam tempus vulputate aliquam.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis tempus ligula eu nullapharetra eleifend. Pellentesque eget nisi gravida, faucibus justo ac, volutpat elit. Praesent egestas posuere elit,et imperdiet magna rhoncus eget. Donec porttitor enim lectus, quis egestas quam dignissim in. Donec dignissim sapienodio, nec molestie enim imperdiet ac. Praesent venenatis quis mi nec pretium.
@@ -21,7 +21,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus consecte
     def test_parser_keep_empty_lines(self):
         # FIXME: There's always a newline at the end of the document, is this okay?
         self.parser = LatexParser(self.document, keep_empty_lines=True)
-        assert self.parser.getResult().getDocument() == """
+        assert self.parser.getResult().getDocument() == r"""
 \documentclass[11pt,a4paper,oneside]{report}
 
 \usepackage{pslatex,palatino,avant,graphicx,color}
@@ -31,9 +31,9 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus consecte
 
 % testtesttesttest
 
-\\begin{document}
-\\title{\color{red}Practical Typesetting}
-\\author{\color{blue}Name}
+\begin{document}
+\title{\color{red}Practical Typesetting}
+\author{\color{blue}Name}
 \date{\color{green}December 2005}
 \maketitle
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus consectetur tellus. Duis vehicula, tortorgravida sollicitudin eleifend, erat eros feugiat nisl, eget ultricies risus magna ac leo. Ut est diam, faucibustincidunt ultrices sit amet, congue sed tellus. Donec vel tellus vitae sem mattis congue. Suspendisse faucibussemper faucibus. Curabitur congue est arcu, nec sollicitudin odio blandit at. Nullam tempus vulputate aliquam.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis tempus ligula eu nullapharetra eleifend. Pellentesque eget nisi gravida, faucibus justo ac, volutpat elit. Praesent egestas posuere elit,et imperdiet magna rhoncus eget. Donec porttitor enim lectus, quis egestas quam dignissim in. Donec dignissim sapienodio, nec molestie enim imperdiet ac. Praesent venenatis quis mi nec pretium.
@@ -44,7 +44,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus consecte
 """
 
     def __init__(self):
-        self.document = """
+        self.document = r"""
 \documentclass[11pt,a4paper,oneside]{report}
 
 \usepackage{pslatex,palatino,avant,graphicx,color}
@@ -57,9 +57,9 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus consecte
       % test
 % test
 
-\\begin{document}
-        \\title{\color{red}Practical Typesetting}
-\\author{\color{blue}Name}
+\begin{document}
+        \title{\color{red}Practical Typesetting}
+\author{\color{blue}Name}
 \date{\color{green}December 2005}
  \maketitle
 
